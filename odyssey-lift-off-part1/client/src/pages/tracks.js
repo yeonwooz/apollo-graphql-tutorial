58908b347a263dad66c4250b0b08fbd117966489
suyeon woo <woosean999@gmail.com>
2023-06-06 23:36:01 +0900

    add.  TRACKS 쿼리 추가

--- odyssey-lift-off-part1/client/src/pages/tracks.js
+++ odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import {Layout} from "../components";
+import {useQuery, gql} from "@apollo/client";
+
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const {loading, error, data} = useQuery(TRACKS);
+
+  if (loading) return "loading...!";
+  if (error) return `ERROR! ${error.message}`;
+  return <Layout grid>{JSON.stringify(data)} </Layout>;
 };
 
 export default Tracks;
