037181c33fd775d81026e8fccd23643f12793fb6
suyeon woo <woosean999@gmail.com>
2023-06-06 23:15:55 +0900

    add. 아폴로 서버 실행코드 추가

--- odyssey-lift-off-part1/server/src/index.js
+++ odyssey-lift-off-part1/server/src/index.js
@@ -1,3 +1,14 @@
 import {ApolloServer} from "@apollo/server";
 import {startStandaloneServer} from "@apollo/server/standalone";
 import typeDefs from "./schema.js";
+
+async function startApolloServer() {
+  const server = new ApolloServer({typeDefs});
+  const {url} = await startStandaloneServer(server);
+  console.log(`
+      🚀  Server is running!
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
