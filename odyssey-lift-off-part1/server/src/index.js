0fb767736803160a2c8525b8d8f99c94087b98e1
suyeon woo <woosean999@gmail.com>
2023-06-06 23:15:09 +0900

    modify. 스키마 보완

--- odyssey-lift-off-part1/server/src/index.js
+++ odyssey-lift-off-part1/server/src/index.js
@@ -1,1 +1,3 @@
-//TODO
+import {ApolloServer} from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone";
+import typeDefs from "./schema.js";
--- odyssey-lift-off-part1/server/src/schema.js
+++ odyssey-lift-off-part1/server/src/schema.js
@@ -2,25 +2,34 @@
 import gql from "graphql-tag";
 
 const typeDefs = gql`
-  type Qeury {
-    "Query to get tracks array for the homepage grid"
-    tracksForHome: [Track!]
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
   }
 
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+    "The track's title"
     title: String!
+    "The track's main author"
     author: Author!
+    "The track's main illustration to display in track card or track page detail"
     thumbnail: String
+    "The track's approximate length to complete, in minutes"
     length: Int
+    "The number of modules this track contains"
     modulesCount: Int
   }
 
-  "Authjor of a complete Track"
+  "Author of a complete Track"
   type Author {
-
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
   }
 `;
 
-module.exports = typeDefs;
+export default typeDefs;
