0624cf76db72ceeb0ef379c008dc5391abfbff91
suyeon woo <woosean999@gmail.com>
2023-06-06 23:30:47 +0900

    add. 아폴로 클라이언트 세팅

--- odyssey-lift-off-part1/client/src/index.js
+++ odyssey-lift-off-part1/client/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+  /*
+   every instance of ApolloClient uses an in-memory cache. 
+   This enables it to store and reuse query results so it doesn't have to make as many network requests. 
+   This makes our app's user experience feel much snappier.
+  */
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
